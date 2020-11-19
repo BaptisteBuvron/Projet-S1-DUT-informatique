82393ef202c6ed3bd003aafb673b317dfc9e1437
Baptiste Buvron <baptiste.buvron@gmail.com>
2020-11-19 16:01:34 +0100

    add navbar responsive

--- js/scriptPage.js
+++ js/scriptPage.js
@@ -1,3 +1,18 @@
 function goBack(){
     window.history.back();
 }
+
+var menu = false;
+
+function menuToggle(){
+    console.log("test")
+    if (menu == false ){
+        $('nav').css("transform", "translateX(0%)");
+        menu = true;
+    }
+    else{
+        $('nav').css("transform", "translateX(-200%)");
+        menu = false;
+    }
+}
+
